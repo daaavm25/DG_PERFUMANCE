12e459d41058fd93b9165c1300cf8e9115170d38
bryan <bmhj76@gmail.com>
2025-10-06 17:19:05 -0600

    interfaces en html

---
+++ Sotfware/pruebas/productos.js
@@ -0,0 +1,78 @@
+const todosLosProductos = [
+    {
+        nombre: 'Eau de perfum',
+        imagen: 'https://ss701.liverpool.com.mx/xl/1137285114.jpg',
+        genero: 'Mujer'
+    },
+    {
+        nombre: 'Scandal',
+        imagen: 'https://media.vogue.mx/photos/635958c1356d41ac3d0b6a5b/master/w_1600%2Cc_limit/Perfumes-Mujeres-Gaultier-Scandal.jpg',
+        genero: 'Mujer'
+    },
+    {
+        nombre: 'Lbeal',
+        imagen: 'https://m.media-amazon.com/images/I/61kVMWOufNL._UF1000,1000_QL80_.jpg',
+        genero: 'Mujer'
+    },
+    {
+        nombre: '212 VIP',
+        imagen: 'https://arome.mx/cdn/shop/files/perfume-212-vip-rose-para-mujer-de-carolina-herrera-eau-de-parfum-arome-mexico-1.jpg?v=1725550285',
+        genero: 'Mujer'
+    },
+    {
+        nombre: 'Dior',
+        imagen: 'https://i.pinimg.com/736x/e3/1f/04/e31f048ef63b7e64658ef03fda6be200.jpg',
+        genero: 'Mujer'
+    },
+    {
+        nombre: 'Dolce && Gabbana',
+        imagen: 'https://arome.mx/cdn/shop/files/perfume-q-para-mujer-de-dolce-and-gabbana-edp-100ml-arome-mexico-1.jpg?v=1705347572',
+        genero: 'Mujer'
+    },
+    {
+        nombre: 'Daisy',
+        imagen: 'https://ss701.liverpool.com.mx/xl/53758509.jpg',
+        genero: 'Mujer'
+    },
+    {
+        nombre: 'Juicy',
+        imagen: 'https://abscents.com.mx/cdn/shop/files/144300_img-7699-juicy-couture-viva-la-juicy-le-bubbly_720-fotor-2024040317822.jpg?v=1712185714&width=1080',
+        genero: 'Mujer'
+    },
+
+    {
+        nombre: 'Valentino',
+        imagen: 'https://i.blogs.es/7fddf9/perfumes-hombre-dia-del-padre-2022-2-/450_1000.jpeg',
+        genero: 'Hombre'
+    },
+    {
+        nombre: 'Hugo Boss',
+        imagen: 'https://fimgs.net/mdimg/perfume/o.383.jpg',
+        genero: 'Hombre'
+    },
+    {
+        nombre: 'Sauvage',
+        imagen: 'https://www.lodoro.cl/cdn/shop/files/Lattafa_11.png?v=1754344432&width=600',
+        genero: 'Hombre'
+    },
+    {
+        nombre: 'Carolina Herrera',
+        imagen: 'https://arome.mx/cdn/shop/files/perfume-ch-sport-para-hombre-de-carolina-herrera-edt-100-ml-arome-mexico-1.jpg?v=1725381849',
+        genero: 'Hombre'
+    },
+    {
+        nombre: 'Paco Rabanne',
+        imagen: 'https://cazanovaonline.mx/cdn/shop/products/pacorabanne1million.jpg?v=1660777896',
+        genero: 'Hombre'
+    },
+    {
+        nombre: 'Jean Paul',
+        imagen: 'https://resources.sanborns.com.mx/imagenes-sanborns-ii/1200/8435415076944.jpg',
+        genero: 'Hombre'
+    },
+    {
+        nombre: 'Yves Saint',
+        imagen: 'https://ss701.liverpool.com.mx/xl/1135832983.jpg',
+        genero: 'Hombre'
+    },
+];
