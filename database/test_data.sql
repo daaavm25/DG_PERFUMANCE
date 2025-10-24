0c1c3bb17b595f10f30783152f465992854ca2cd
Ghost63s <grimaldodiego380@gmail.com>
2025-10-24 11:50:27 -0600

    agregaron registros2

--- database/test_data.sql
+++ database/test_data.sql
@@ -8,3 +8,23 @@ INSERT INTO gestion_perfumance.rol (descripcion) VALUES
 INSERT INTO gestion_perfumance.genero (descripcion) VALUES
 ('Hombre'),
 ('Mujer');
+
+INSERT INTO gestion_perfumance.perfume
+(marca, presentacion, talla, id_genero, stock, fecha_caducidad)
+VALUES
+('Coach Green', 'Eau de Parfum', '100ml', 3, 10, '2030-12-31'),
+('Victoria Secret Bombshell', 'Eau de Parfum', '100ml', 2, 12, '2030-12-31'),
+('Victoria Secret Champagne', 'Eau de Parfum', '100ml', 2, 8, '2030-12-31'),
+('Montblanc Explorer', 'Eau de Parfum', '100ml', 1, 9, '2030-12-31'),
+('Versace Eros', 'Eau de Toilette', '100ml', 1, 11, '2030-12-31'),
+('Acqua di Giò', 'Eau de Toilette', '100ml', 1, 10, '2030-12-31'),
+('Jean Paul Elixir', 'Eau de Parfum', '100ml', 1, 7, '2030-12-31'),
+('Yves Saint Laurent Y', 'Eau de Parfum', '100ml', 1, 6, '2030-12-31');
+
+
+
+ INSERT INTO gestion_perfumance.genero (descripcion)
+VALUES
+('Masculino'),
+('Femenino'),
+('Unisex');
